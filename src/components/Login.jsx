a0e79caf080fc25fdef63f7af9d6ab5c601b4bc6
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-05-28 16:53:10 +0600

    ui part updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,36 @@
-import {Redirect, Link} from "react-router-dom";
-import React from "react";
-import {CLIENT_ID} from "../redux/actions/actions";
+import styled from "styled-components"
 
+let Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: rgb(25,25,24);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    gap: 20px;
+    .title {
+        font-size: 25px;
+    }
+    button {
+        background: rgb(29,184,85);
+        text-transform: uppercase;
+        padding: 10px 20px;
+        border: none;
+        color: white;
+        border-radius: 20px;
+        cursor: pointer;
+    }
+`
 export default function Login(props) {
-    let redirectUri = 'localhost:3000'
-    let endcodedUri = encodeURI(redirectUri)
-
+    let myURL = encodeURI("http://localhost:3000/callback");
+    let clientId = "2cede5eb11ad40a7957d539e35fed5d7";
+    let spotify = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${myURL}&scope=user-read-private%20user-read-email&state=34fFs29kd09`
     return (
-        <a
-            href={`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2F${endcodedUri}%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09`}>Login</a>
+        <Container>
+            <div className="title">Spotify Profile</div>
+            <a href={spotify}> <button>Log In To Spotify</button></a>
+        </Container>
     )
-
 }
